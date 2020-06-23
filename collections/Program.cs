31032b9b3b91b97a255f713e5bd9c4bfc1baebb8
immanuelyofi21 <immanuel.manafe@students.amikom.ac.id>
2020-06-23 19:38:31 +0700

    first commit

---
+++ collections/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using collections.classInduk;
+using collections.classAnak;
+
+namespace collections
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            karyawanTetap karyawanTetap = new karyawanTetap();
+            karyawanTetap.Nik = "19.11.2737";
+            karyawanTetap.Nama = "Immanuel";
+            karyawanTetap.GajiBulanan = 10000000;
+
+            karyawanHarian karyawanHarian = new karyawanHarian();
+            karyawanHarian.Nik = "19.11.4343";
+            karyawanHarian.Nama = "Paulus";
+            karyawanHarian.JumlahJamKerja = 15;
+            karyawanHarian.UpahPerJam = 55000;
+
+            sales sales = new sales();
+            sales.Nik = "19.11.7373";
+            sales.Nama = "Claudio";
+            sales.JumlahPenjualan = 50;
+            sales.Komisi = 50000;
+
+            List<karyawan> listKaryawan = new List<karyawan>();
+
+            listKaryawan.Add(karyawanTetap);
+            listKaryawan.Add(karyawanHarian);
+            listKaryawan.Add(sales);
+
+            int noUrut = 1;
+
+            foreach(karyawan karyawan in listKaryawan)
+            {
+                Console.WriteLine("{0}. Nik : {1}, Nama : {2}, Gaji : {3:N0}", noUrut, karyawan.Nik, karyawan.Nama, karyawan.Gaji());
+            }
+        }
+    }
+}
